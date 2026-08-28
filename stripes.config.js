4794a8d83b226f90252b8bc83bbfdd8128b55b87
eldiiar-duishenaliev <eldiiar.duishenaliev@gmail.com>
2026-08-28 11:00:40 +0600

    RANCHER-3134 UI fed tests v1

--- stripes.config.js
+++ stripes.config.js
@@ -1,7 +1,3 @@
-const modules = require("./stripes.modules");
-const extra = require("./stripes.extra");
-const { merge } = require('lodash');
-
 // Documentation: https://folio-org.atlassian.net/wiki/spaces/DEV/pages/46858271/stripes.config.js+properties
 module.exports = {
   okapi: {
@@ -29,7 +25,7 @@ module.exports = {
     preserveConsole: true,
     useSecureTokens: true,
   },
-  modules: merge({}, modules, extra),
+  modules: {},
   branding: {
     logo: {
       src: './tenant-assets/opentown-libraries-logo.png',
